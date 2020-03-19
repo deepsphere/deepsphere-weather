240ddfecbae2bfda47ca5ad0f411cc151af355f9
illorens <iciarllorens@gmail.com>
2020-03-19 19:27:28 +0100

    changed class names

--- modules/data.py
+++ modules/data.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import numpy as np
+import os
 
 import torch
 from torch import nn, optim 
@@ -7,26 +8,30 @@ from torch.utils.data import Dataset
 
 
 # Test data
-def load_test_data(path, var, years=slice('2017', '2018')):
-    """
-    Args:
-        path: Path to nc files
-        var: variable. Geopotential = 'z', Temperature = 't'
-        years: slice for time window
-    Returns:
-        dataset: Concatenated dataset for 2017 and 2018
-    """
-    assert var in ['z', 't'], 'Test data only for Z500 and T850'
-    ds = xr.open_mfdataset(f'{path}/*.nc', combine='by_coords')[var]
+def load_test_data(path, delta_t, years=slice('2017', '2018')):
+    zpath = path + '/geopotential_500'
+    tpath = path + '/temperature_850'
+    
+    z = xr.open_mfdataset(zpath+'/*.nc', combine='by_coords')['z']
+    t = xr.open_mfdataset(tpath+'/*.nc', combine='by_coords')['t']
+
     try:
-        ds = ds.sel(level=500 if var == 'z' else 850).drop('level')
+        z = z.drop('level')
     except ValueError:
         pass
-    return ds.sel(time=years)
+
+    try:
+        t = t.drop('level')
+    except ValueError:
+        pass
+
+    dataset = xr.merge([z, t], compat='override')
+
+    return dataset.sel(time=years).isel(time=slice(delta_t, None))
 
 
 # Datasets
-class Weather_Dataset(Dataset):
+class WeatherDataset(Dataset):
     
     def __init__(self, datadir, variables, level, years, lead_time, load=True, mean=None, std=None):
         
@@ -74,16 +79,53 @@ class Weather_Dataset(Dataset):
 
     def __getitem__(self, idx):
         idxs = self.idxs[idx]
-        X = self.data.isel(time=idxs).values
-        y = self.data.isel(time=idxs + self.lead_time).values
-        
-        X = torch.Tensor(X).permute(1, 0)
-        y = torch.Tensor(y).permute(1, 0)
+        X = torch.Tensor(self.data.isel(time=idxs).values)
+        y = torch.Tensor(self.data.isel(time=idxs + self.lead_time).values)
 
+        return X, y
+    
+class WeatherBenchDataset1dNumpy(Dataset):
+    
+    def __init__(self, data_path, lead_time, lat, lon, var_dict, valid_time, mean=None, std=None):
+        
+        self.lead_time = lead_time
+        self.lat = lat
+        self.lon = lon
+        self.var_dict = var_dict
+        self.valid_time = valid_time
+    
+        self.features = len(self.var_dict)
+        self.latitudes = len(self.lat)
+        self.longitudes = len(self.lon)
+        
+        self.mean = np.load(data_path + 'mean.npy') if mean is None else mean
+        self.std = np.load(data_path + 'std.npy') if std is None else std
+
+        
+        total_samples = len(os.listdir(data_path)) - 2
+        self.n_samples = total_samples - self.lead_time
+        
+        self.datafiles = [(data_path+str(id)+'.npy', data_path+str(id+lead_time)+'.npy')
+                           for id in list(range(self.n_samples))]
+        
+    def __len__(self):
+        return self.n_samples
+    
+    def __getitem__(self, idx):
+        """ Returns sample and label corresponding to an index as torch.Tensor objects
+            The return tensor shapes are (for the sample and the label): [n_vertex, n_features]
+        """
+        
+        '''X = self.transform(np.load(self.datafiles[idx]))
+        y = self.transform(np.load(self.datafiles[idx+delta_t]))'''
+        
+        X = torch.Tensor((np.load(self.datafiles[idx][0])-self.mean)/self.std)
+        y = torch.Tensor((np.load(self.datafiles[idx][1])-self.mean)/self.std)
+        
         return X, y
 
 
-class Dataset_WeatherBench_1D(Dataset):
+class WeatherBenchDataset1dXarray(Dataset):
     
     def __init__(self, ds, var_dict, lead_time, load=True, mean=None, std=None):
         
@@ -110,26 +152,33 @@ class Dataset_WeatherBench_1D(Dataset):
         self.valid_time = self.data.isel(time=slice(lead_time, None)).time
         self.idxs = np.arange(self.n_samples)
         
+        self.lat = self.data.lat
+        self.lon = self.data.lon
+        
+        self.features = len(self.data.level)
+        self.latitudes = len(self.lat)
+        self.longitudes = len(self.lon)
+        
         # Stack
         self.data = self.data.stack(nodes=('lat', 'lon')).transpose('time', 'nodes', 'level')
         
-        if load: print('Loading data into RAM'); self.data.load()
+        # For some weird reason calling .load() earlier messes up the mean and std computations
+        if load: 
+            print('Loading data into RAM')
+            self.data.load()
         
     def __len__(self):
         return self.n_samples
 
     def __getitem__(self, idx):
         idxs = self.idxs[idx]
-        X = self.data.isel(time=idxs).values
-        y = self.data.isel(time=idxs + self.lead_time).values
-        
-        X = torch.Tensor(X).permute(1, 0)
-        y = torch.Tensor(y).permute(1, 0)
+        X = torch.Tensor(self.data.isel(time=idxs).values)
+        y = torch.Tensor(self.data.isel(time=idxs + self.lead_time).values)
 
         return X, y
 
     
-class Dataset_WeatherBench_2D(Dataset):
+class WeatherBenchDataset2d(Dataset):
     
     def __init__(self, ds, var_dict, lead_time, load=True, mean=None, std=None):
 
