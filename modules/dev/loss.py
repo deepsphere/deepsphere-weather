04f2c65e45e5c2c2282bfd26385a7269a9e8d050
wefeng <wentao.feng@epfl.ch>
2021-01-17 21:30:37 +0100

    add mse loss supporting user-defined mask/weights

--- modules/dev/loss.py
+++ modules/dev/loss.py
@@ -8,6 +8,10 @@ Created on Sun Jan  3 23:59:25 2021
 import numpy as np 
 import matplotlib.pyplot as plt
 
+import torch
+from torch import nn
+from torch.nn import functional as F
+
 #############################
 ### Weight updating rule ####
 #############################
@@ -42,3 +46,44 @@ def plot_weight_variations(weights_loss, figures_path):
     plt.ylabel('Weight')
     plt.tight_layout()
     plt.savefig(figures_path + 'weight_updates.pdf')
+
+
+class WeightedMSELoss(nn.MSELoss):
+    def __init__(self, reduction='sum', weights=None, normalized=False):
+        super(WeightedMSELoss, self).__init__(reduction='none')
+        if not isinstance(reduction, str) or reduction not in ('mean', 'sum', 'none'):
+            raise ValueError("{} is not a valid value for reduction".format(reduction))
+        self.weighted_mse_reduction = reduction
+
+        if weights is not None:
+            self.check_weights(weights)
+            if normalized:
+                weights /= torch.sum(weights)
+        self.weights = weights
+    
+    def forward(self, pred, label, weights=None):
+        mse = super(WeightedMSELoss, self).forward(pred, label)
+        if weights is None:
+            weights = self.weights
+        else:
+            self.check_weights(weights)
+        
+        _, num_nodes, _ = mse.shape
+        if weights is None:
+            weights = torch.ones((num_nodes), dtype=mse.dtype, device=mse.device)
+        if num_nodes != len(weights):
+            raise ValueError("The number of weights does not match the the number of pixels. {} != {}"
+                                .format(len(weights), num_nodes))
+        weights = weights.view(1, -1, 1).to(mse.device)
+        if self.weighted_mse_reduction == 'sum':
+            return torch.sum(mse * weights)
+        elif self.weighted_mse_reduction == 'mean':
+            return torch.mean(mse * weights)
+        else:
+            return mse * weights
+
+    def check_weights(self, weights):
+        if not isinstance(weights, torch.Tensor):
+            raise TypeError("Weights type is not a torch.Tensor. Got {}".format(type(weights)))
+        if len(weights.shape) != 1:
+            raise ValueError("Weights is a 1D vector. Got {}".format(weights.shape))
