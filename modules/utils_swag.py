0e02350d62e196a0a9102ed56fe3b086553cf67d
Yasser Haddad <yann.haddad@epfl.ch>
2021-03-14 15:01:31 +0100

    Implementing SWAG in the new pipeline

---
+++ modules/utils_swag.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import itertools
+import torch
+import os
+import copy
+from datetime import datetime
+import math
+import numpy as np
+import tqdm
+
+import torch.nn.functional as F
+
+def flatten(lst):
+    tmp = [i.contiguous().view(-1, 1) for i in lst]
+    return torch.cat(tmp).view(-1)
+
+
+def unflatten_like(vector, likeTensorList):
+    # Takes a flat torch.tensor and unflattens it to a list of torch.tensors
+    #    shaped like likeTensorList
+    outList = []
+    i = 0
+    for tensor in likeTensorList:
+        # n = module._parameters[name].numel()
+        n = tensor.numel()
+        outList.append(vector[:, i : i + n].view(tensor.shape))
+        i += n
+    return outList
+
+
+def _check_bn(module, flag):
+    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
+        flag[0] = True
+
+
+def check_bn(model):
+    flag = [False]
+    model.apply(lambda module: _check_bn(module, flag))
+    return flag[0]
+
+
+def reset_bn(module):
+    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
+        module.running_mean = torch.zeros_like(module.running_mean)
+        module.running_var = torch.ones_like(module.running_var)
+
+
+def _get_momenta(module, momenta):
+    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
+        momenta[module] = module.momentum
+
+
+def _set_momenta(module, momenta):
+    if issubclass(module.__class__, torch.nn.modules.batchnorm._BatchNorm):
+        module.momentum = momenta[module]
+
+
+def bn_update(loader, model, batch_size, device, verbose=False, **kwargs):
+    """
+        BatchNorm buffers update (if any).
+        Performs 1 epochs to estimate buffers average using train dataset.
+        :param loader: train dataset loader for buffers average estimation.
+        :param model: model being update
+        :return: None
+    """
+    if not check_bn(model):
+        return
+    model.train()
+    momenta = {}
+    model.apply(reset_bn)
+    model.apply(lambda module: _get_momenta(module, momenta))
+    n = 0
+    num_batches = len(loader)
+    n_samples = loader.n_samples
+    idxs = loader.idxs
+
+    with torch.no_grad():
+        if verbose:
+            loader = tqdm.tqdm(loader, total=num_batches)
+
+        for i in range(0, n_samples - batch_size, batch_size):
+            i_next = min(i + batch_size, n_samples)
+            
+            input_var = torch.autograd.Variable(input)
+            b = input_var.data.size(0)
+
+            momentum = b / (n + b)
+            for module in momenta.keys():
+                module.momentum = momentum
+
+            model(input_var, **kwargs)
+            n += b
+
+    model.apply(lambda module: _set_momenta(module, momenta))
