8af765e6e8602a26b92a4e1329b5dc3884ff4d9e
ghiggi <gionata.ghiggi@gmail.com>
2021-08-26 16:05:53 +0000

    Code adaptation for tensor_info changes

--- modules/my_models_graph.py
+++ modules/my_models_graph.py
@@ -19,6 +19,8 @@ from modules.utils_models import check_pool_method
 from modules.utils_models import check_skip_connection
 from modules.utils_models import pygsp_graph_coarsening
 
+# https://towardsdatascience.com/resnet-with-identity-mapping-over-1000-layers-reached-image-classification-bb50a42af03e 
+
 ##----------------------------------------------------------------------------.
 class ConvBlock(GeneralConvBlock):
     """Spherical graph convolution block.
@@ -102,13 +104,11 @@ class ConvBlock(GeneralConvBlock):
         x = self.conv(x)
         if self.norm and self.batch_norm_before_activation:   
             # [batch, node, time-feature]
-            print(self.norm)
             x = self.bn(x.permute(0, 2, 1)).permute(0, 2, 1)
         if self.act:
             x = self.act_fun(x)
         if self.norm and not self.batch_norm_before_activation:   
             # [batch, node, time-feature]
-            print(self.norm)
             x = self.bn(x.permute(0, 2, 1)).permute(0, 2, 1)
         return x
         
@@ -175,8 +175,8 @@ class UNetSpherical(UNet, torch.nn.Module):
         
     Parameters
     ----------
-    dim_info: dict
-        Dictionary with all the relevant informations (i.e. shape, dimension order)
+    tensor_info: dict
+        Dictionary with all relevant shape, dimension and feature order informations 
         regarding input and output tensors.
     sampling : str
         Name of the spherical sampling.
@@ -239,7 +239,7 @@ class UNetSpherical(UNet, torch.nn.Module):
     """
 
     def __init__(self,
-                 dim_info: Dict[str, int],
+                 tensor_info: Dict,
                  sampling: str,
                  sampling_kwargs: Dict, 
                  knn: int = 10, 
@@ -267,18 +267,28 @@ class UNetSpherical(UNet, torch.nn.Module):
         super().__init__()
         ##--------------------------------------------------------------------.
         # Retrieve tensor informations 
-        self.dim_names = dim_info['dim_order']
-        self.input_feature_dim = dim_info['input_feature_dim']
-        self.input_time_dim = dim_info['input_time_dim']
-        self.input_node_dim = dim_info['input_node_dim'] 
+        self.dim_names = tensor_info['dim_order']['dynamic']  
         
-        self.output_time_dim = dim_info['output_time_dim']
-        self.output_feature_dim = dim_info['output_feature_dim']
-        self.output_node_dim = dim_info['output_node_dim']   
+        # dim_info = tensor_info['dynamic']['dim_info']  
+        # feature_static = tensor_info['feature_order']['static']
+        # feature_bc = tensor_info['feature_order']['bc']
+        # feature_dynamic = tensor_info['feature_order']['dynamic']
+        # input_features =  feature_static + feature_bc + feature_dynamic
+        # output_features = feature_dynamic 
         
-        self.input_channels = dim_info['input_feature_dim']*dim_info['input_time_dim']
-        self.output_channels = dim_info['output_feature_dim']*dim_info['output_time_dim']
+        self.input_n_feature = tensor_info['input_n_feature']
+        self.output_n_feature = tensor_info['output_n_feature']
+        self.input_n_time = tensor_info['input_n_time']
+        self.output_n_time = tensor_info['output_n_time']
+        self.input_node_dim = tensor_info['input_shape_info']['dynamic']['node']  
+        self.output_node_dim = tensor_info['output_shape_info']['dynamic']['node']  
         
+        ##--------------------------------------------------------------------.
+        # Define size of last dimension for ConvChen conv (merging time-feature dimension)
+        self.input_channels = self.input_n_feature*self.input_n_time
+        self.output_channels = self.output_n_feature*self.output_n_time
+        
+        ##--------------------------------------------------------------------.
         # Decide whether to predict the difference from the previous timestep 
         #  instead of the full state 
         self.increment_learning = increment_learning
@@ -398,19 +408,18 @@ class UNetSpherical(UNet, torch.nn.Module):
         """Define UNet encoder."""
         # Recommended optimal shape: [sample, node, time, feature]
         # --> Contiguous inputs can be reshaped without copying --> ['sample', 'node', 'time-feature']
-        
         ##--------------------------------------------------------------------.
         batch_size = x.shape[0]
-    
         ##--------------------------------------------------------------------.
         ## Extract last timestep (to add after decoding) to make the network learn the increment
         x_last_timestep = x[:,-1,:,-2:].unsqueeze(dim=1)
         
         ##--------------------------------------------------------------------.
-        # Reorder and reshape data 
-        x = x.rename(*self.dim_names).align_to('sample','node','time','feature').rename(None) # x.permute(0, 2, 1, 3)   
+        # Reorder and reshape data to ['sample', 'node', 'time-feature'] --> [B, V, Fin] for ConvCheb
+        x = x.rename(*self.dim_names).align_to('sample','node','time','feature').rename(None)   
         x = x.reshape(batch_size, self.input_node_dim, self.input_channels)  # reshape to ['sample', 'node', 'time-feature'] 
         ##--------------------------------------------------------------------.
+        ## Define encoder computation
         # Level 1
         x_enc1 = self.conv1(x)
 
@@ -427,6 +436,7 @@ class UNetSpherical(UNet, torch.nn.Module):
     ##------------------------------------------------------------------------.
     def decode(self, x_enc3, x_enc2, x_enc1, idx2, idx1, x_last_timestep): 
         """Define UNet decoder."""
+        ## Define decoder computation
         # - Level 2
         x = self.unpool2(x_enc3, idx2)
         x_cat = torch.cat((x, x_enc2), dim=2)
@@ -442,9 +452,10 @@ class UNetSpherical(UNet, torch.nn.Module):
         
         ##--------------------------------------------------------------------.
         # Reshape data to ['sample', 'time', 'node', 'feature']
-        batch_size = x.shape[0]   # ['sample', 'node', 'time-feature'] 
-        x = x.reshape(batch_size, self.output_node_dim, self.output_time_dim, self.output_feature_dim)   # ==> ['sample', 'node', 'time', 'feature']
-        x = x.rename(*['sample', 'node', 'time', 'feature']).align_to(*self.dim_names).rename(None) # x.permute(0, 2, 1, 3)  
+        batch_size = x.shape[0]   # 
+        # Reshape from ['sample', 'node', 'time-feature']  to ['sample', 'node', 'time', 'feature']
+        x = x.reshape(batch_size, self.output_node_dim, self.output_n_time, self.output_n_feature)   
+        x = x.rename(*['sample', 'node', 'time', 'feature']).align_to(*self.dim_names).rename(None)  
         ##--------------------------------------------------------------------.
         # Add tensor of most recent past timestep
         if self.increment_learning:
